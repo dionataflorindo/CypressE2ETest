15aa059ddc2a3ba537fe07eeb4fa435d337ae527
dionataflorindo <florindodionata12@gmail.com>
2024-04-13 12:49:50 -0300

    1

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e",
     baseUrl:"https://automationexercise.com/",
   },
   video:true,
