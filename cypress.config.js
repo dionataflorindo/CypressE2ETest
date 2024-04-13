69824f181812ebcbdc762ffcb27a9c6f01afeed6
dionataflorindo <florindodionata12@gmail.com>
2024-04-13 17:30:32 -0300

    vai nada

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    reporter: "junit",
-    reporterOptions: {
-    mochaFile: "cypress/reports/junit/test-results-[hash].xml",
-    toConsole : true,
-    },
     specPattern: "cypress/e2e",
     baseUrl:"https://automationexercise.com/",
   },
